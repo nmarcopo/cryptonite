7caaf0432c15cdc0156af8ca36bacb5f16a5ac63
Nicholas Marcopoli <nmarcopo@nd.edu>
2018-11-05 22:45:26 -0500

    inital commit crypto

---
+++ crypto_api.py
@@ -0,0 +1,69 @@
+import requests
+import json
+
+# Nick Marcopoli, just him
+
+class _crypto_api:
+    def __init__(self):
+        self.apiBaseURL = "https://min-api.cryptocompare.com/data/"
+        self.top15List = ["BTC","ETH","XRP","BCH","EOS","XLM","LTC","ADA","XMR","USDT","TRX","DASH","IOTA","TRX","DASH"]
+        
+    def find_hottest_coldest(self, days, topN, mode):
+        # get top 10 most increased value crypto
+        # calculated by doing open on first day to close on last day
+        coinDict = {}
+        for item in self.top15List:
+            crypto_hottest_url = self.apiBaseURL + "histoday?fsym={}&tsym=USD&limit={}".format(item, days)
+            r = requests.get(crypto_hottest_url)
+            resp = json.loads(r.content)
+            cryptoDataDaysAgo = resp['Data'][0]['open']
+            if cryptoDataDaysAgo == 0:
+                #print(item)
+                continue
+            cryptoDataToday = resp['Data'][days]['close']
+            hotMeasurement = (cryptoDataToday - cryptoDataDaysAgo) / cryptoDataDaysAgo * 100
+            coinDict[item] = hotMeasurement
+        if mode == "hot":
+            sorted_by_value = sorted(coinDict.items(), key=lambda kv: kv[1], reverse=True)
+        elif mode == "cold":
+            sorted_by_value = sorted(coinDict.items(), key=lambda kv: kv[1], reverse=False)
+        else:
+            print("please type 'hot' or 'cold'")
+            return
+        #print(sorted_by_value)
+        for item, val in sorted_by_value[0:topN]:
+            print("{}: {:.2f}%".format(item, val))
+
+
+    def what_if_investment(self, days, cryptosAndAmount):
+        totalMoneyGained = 0
+        crypto_code = []
+        investment = []
+        current = []
+        for crypto, amount in cryptosAndAmount.items():
+            crypto_choice_url = self.apiBaseURL + "histoday?fsym={}&tsym=USD&limit={}".format(crypto, days)
+            r = requests.get(crypto_choice_url)
+            resp = json.loads(r.content)
+            cryptoDataDaysAgo = resp['Data'][0]['open']
+            if cryptoDataDaysAgo == 0:
+                print("This crypto, {}, didn't exist {} days ago. Not computing with this crypto.".format(crypto, days))
+                continue 
+            cryptoDataToday = resp['Data'][days]['close']
+            crypto_code.append(crypto)
+            investment.append(cryptoDataDaysAgo * amount)
+            current.append(cryptoDataToday * amount)
+       
+        totalMoneyGained = sum(current) - sum(investment)
+        percentChange = totalMoneyGained / sum(investment) * 100
+        print("Net change: ${:.2f}, {:.2f}%".format(totalMoneyGained, percentChange))
+        print()
+        for i in range(len(crypto_code)):
+            print("{}: {:.2f} {:.2f}%".format(crypto_code[i], current[i] - investment[i], (current[i] - investment[i]) / investment[i] * 100)) 
+
+
+
+
+
+if __name__ == "__main__":
+    test = _crypto_api()
+    test.what_if_investment(100, {'BTC':2, 'DBC':100})
