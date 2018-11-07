b91dfe6225b9688e0524d10d2f29035450533f56
Administrator <csong1@nd.edu>
2018-11-06 21:35:43 -0500

    10 points for Luke, userDB

---
+++ _user_database.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# User database
+# Luke Song, Nick Marcopoli, Andy Shin, Austin Sura
+
+import hashlib
+
+class _user_database:
+    def __init__:
+        self.user_pwd = {}
+        self.user_wallet = {}
+
+    def set_user(self, user, pwd):
+        self.user_pwd[user] = hashlib.sha1(pwd.encode()).hexdigest()
+        self.user_wallet[user] = []
+
+    def check_pwd(self, user, pwd):
+        if self.user_pwd[user] == hashlib.sha1(pwd.encode()).hexdigest():
+            return True
+        else:
+            return False
+
+    def change_pwd(self, user, curr_pwd, new_pwd):
+        if self.user_pwd[user] == hashlib.sha1(curr_pwd.encode()).hexdigest():
+            self.user_pwd[user] = hashlib.sha1(new_pwd.encode()).hexdigest()
+    
+    def change_id(self, user, newID, pwd):
+        if self.user_pwd[user] == hashlib.sha1(pwd.encode()).hexdigest():
+            self.user_pwd[newID] = self.user_pwd.pop(user)
+
+    def delete_user(self, user, pwd):
+        if user in self.user_pwd:
+            if self.user_pwd[user] == hashlib.sha1(pwd.encode()).hexdigest()
+                del self.user_pwd[user]
+        if user in self.user_wallet:
+            del self.user_wallet[user]
+
+    def add_sub_asset(self, user, asset_dict):
+        for coin, amount in asset_dict.items():
+            for asset, count in self.user_wallet.items():
+                if asset == coin:
+                    self.user_wallet[asset] = count + amount
