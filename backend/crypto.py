8152fdee024edc3509c364fc9ce8760e6d365be1
SungHyun Shin <sshin1@student04.cse.nd.edu>
2018-11-28 00:54:02 -0500

    added try block

--- backend/crypto.py
+++ backend/crypto.py
@@ -10,35 +10,41 @@ class CryptoController:
     
     # Get top hottest or coldest crypto
     def PUT_TOPN(self):
-        payload = cherrypy.request.body.read()
-        data = json.loads(payload)
-        temp = data['temp']
-        days = data['days']
-        count = data['count']
-        static = data['static']
-        if static:
-            with open('hotcold.dat') as f:
-                preloaded = f.readline().strip()
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        response = loop.run_until_complete(self.crypto.find_hottest_coldest(days, count, temp, json.loads(preloaded)))
+        try:
+            payload = cherrypy.request.body.read()
+            data = json.loads(payload)
+            temp = data['temp']
+            days = data['days']
+            count = data['count']
+            static = data['static']
+            if static:
+                with open('hotcold.dat') as f:
+                    preloaded = f.readline().strip()
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            response = loop.run_until_complete(self.crypto.find_hottest_coldest(days, count, temp, json.loads(preloaded)))
 
-        output = json.loads(response)
-        output['result'] = 'success'
+            output = json.loads(response)
+            output['result'] = 'success'
+        except:
+            output['result'] = 'error'
         return json.dumps(output)
     
     # Do what if investment
     def PUT(self, days):
-        output = {}
-        payload = cherrypy.request.body.read()
-        data = json.loads(payload)
-        asset = data['asset'][0]                # data['asset'] is a list
-        with open('whatif.dat') as f:
-            preloaded = f.readline().strip()
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        response = loop.run_until_complete(self.crypto.what_if_investment(int(days), asset, json.loads(preloaded)))
+        try:
+            output = {}
+            payload = cherrypy.request.body.read()
+            data = json.loads(payload)
+            asset = data['asset'][0]                # data['asset'] is a list
+            with open('whatif.dat') as f:
+                preloaded = f.readline().strip()
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            response = loop.run_until_complete(self.crypto.what_if_investment(int(days), asset, json.loads(preloaded)))
 
-        output = json.loads(response)
-        output['result'] = 'success'
+            output = json.loads(response)
+            output['result'] = 'success'
+        except:
+            output['result'] = 'error'
         return json.dumps(output) 
