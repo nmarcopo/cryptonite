3a22e84735dba93a7473ba2087b42a4bd13c4c0d
nmarcopo <nmarcopo@nd.edu>
2018-11-27 21:15:48 -0500

    added basic login functionality

--- frontend/web_client/js/index.js
+++ frontend/web_client/js/index.js
@@ -1,16 +1,37 @@
-// Support submit button for login
+// Auto run when index.html Welcome page is loaded
+$(function () {
+    $("#modalInsert").load("res/modalSignUp.html", function () {
+        document.getElementById("signUpSubmitButton").addEventListener("click", signUpSubmit);
+        document.getElementById("logInSubmitButton").addEventListener("click", logInSubmit);
+    });
+})
+
+// Support submit button for sign up
 function signUpSubmit() {
+    // Get nodes for inputs
     var uidNode = document.getElementById("inputEmailSignUp");
     var passNode = document.getElementById("inputPasswordSignUp");
     var passConfNode = document.getElementById("inputPasswordSignUpConfirm");
+    // Get node of containing modal for appending messages to
     var loginContainer = uidNode.parentNode.parentNode;
 
+    // Get the text that was typed in the inputs
     var uid = uidNode.value;
     var pass = passNode.value;
     var passConf = passConfNode.value;
 
-    if (pass == passConf) {
-        var putRequest = {
+    // Check if email field does not contain an email
+    // regex source: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(uid).toLowerCase())) {
+        createNotification("danger", loginContainer, "Your email address is not valid. Please enter a valid email.");
+        return;
+    }
+
+    // Check if passwords match
+    if (pass == passConf && uid != "" && pass != "") {
+        // Send POST request to create a new user
+        var postRequest = {
             'user': uid,
             'pwd': pass
         }
@@ -25,42 +46,82 @@ function signUpSubmit() {
                 uidNode.setAttribute("readonly", '""');
                 passNode.setAttribute("readonly", '""');
                 passConfNode.setAttribute("readonly", '""');
-                createNotification("green", loginContainer);
+                createNotification("success", loginContainer, "Well Done! You successfully created an account. Please close this window and log in.");
             } else {
-                // do something else
+                // fails when there is already an existing user
                 console.log('failure');
-                createNotification("red", loginContainer);
+                createNotification("danger", loginContainer, "Oh snap! Username already exists. Try logging in, or creating a new user.");
+            }
+        }
+        xhr_putNewUser.send(JSON.stringify(postRequest));
+    } else {
+        createNotification("danger", loginContainer, "Oh snap! Change a few things up and try submitting again.");
+    }
+}
+
+// Handle login
+function logInSubmit() {
+    // put request to /users/, send json user, pwd
+    // Get nodes for inputs
+    var uidNode = document.getElementById("inputEmailLogIn");
+    var passNode = document.getElementById("inputPasswordLogIn");
+    // Get node of containing modal for appending messages to
+    var loginContainer = uidNode.parentNode.parentNode;
+
+    // Get the text that was typed in the inputs
+    var uid = uidNode.value;
+    var pass = passNode.value;
+
+    // Check if email field does not contain an email
+    // regex source: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!re.test(String(uid).toLowerCase())) {
+        createNotification("danger", loginContainer, "Your email address is not valid. Please enter a valid email.");
+        return;
+    }
+
+    if (pass != "") {
+        // Send request to login to server
+        var putRequest = {
+            'user': uid,
+            'pwd': pass
+        }
+        var xhr_putNewUser = new XMLHttpRequest();
+        xhr_putNewUser.open("PUT", 'http://student04.cse.nd.edu:52109/users/');
+        xhr_putNewUser.onload = function (e) {
+            responseDict = JSON.parse(xhr_putNewUser.responseText);
+            console.log(xhr_putNewUser.responseText);
+            if (responseDict['result'] == 'success') {
+                // give success message, make sure user can't immediately request another username
+                console.log("success");
+                uidNode.setAttribute("readonly", '""');
+                passNode.setAttribute("readonly", '""');
+                createNotification("success", loginContainer, "Well Done! You successfully logged in.");
+            } else {
+                // fails when there is already an existing user
+                console.log('failure');
+                createNotification("danger", loginContainer, "Oh snap! Your information wasn't correct.");
             }
         }
         xhr_putNewUser.send(JSON.stringify(putRequest));
     } else {
-        createNotification("red", loginContainer);
+        createNotification("danger", loginContainer, "Please enter a password.");
     }
 }
-$(function () {
-    $("#modalInsert").load("res/modalSignUp.html", function () {
-        document.getElementById("signUpSubmitButton").addEventListener("click", signUpSubmit);
-    });
-})
 
-function createNotification(color, loginContainer) {
-    var messageHTML;
-    if (color == "red") {
-        messageHTML = `<div class="alert alert-dismissible alert-danger">
-        <button type="button" class="close" data-dismiss="alert">&times;</button>
-        <strong>Oh snap!</strong> Change a few things up and try submitting again.
-        </div>`
-    } else if (color == "green") {
-        messageHTML = `<div class="alert alert-dismissible alert-success">
-        <button type="button" class="close" data-dismiss="alert">&times;</button>
-        <strong>Well done!</strong> You successfully read this important alert message.
-        </div>`
-    }
-
+// Function to create a notification pop up 
+function createNotification(alertType, loginContainer, messageText) {
+    var messageHTML = `<div class="alert alert-dismissible alert-${alertType}">
+    <button type="button" class="close" data-dismiss="alert">&times;</button>
+    ${messageText}
+    </div>`;
     var message = document.createElement("div");
     message.innerHTML = messageHTML;
     loginContainer.appendChild(message);
+
+    // Automatically delete the message after 5 seconds.
+    var timeoutSecs = 5
     setTimeout(() => {
         loginContainer.removeChild(message);
-    }, 5000);
+    }, timeoutSecs * 1000);
 }
