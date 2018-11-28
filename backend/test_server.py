88c54504ffee294fe2b09eb5c51b6f2ace6369d8
SungHyun Shin <sshin1@student04.cse.nd.edu>
2018-11-27 23:19:08 -0500

    crypto done

--- backend/test_server.py
+++ backend/test_server.py
@@ -2,10 +2,11 @@ import unittest
 import requests
 import json
 
-class TestMovies(unittest.TestCase):
+class TestServer(unittest.TestCase):
 
-	SITE_URL = 'http://student04.cse.nd.edu:52109' # replace this with your port number
+	SITE_URL = 'http://student04.cse.nd.edu:52107' # replace this with your port number
 	USERS_URL = SITE_URL + '/users/'
+	CRYPTO_URL = SITE_URL + '/crypto/'
 	RESET_URL = SITE_URL + '/reset/'
 
 	def reset_data(self):
@@ -25,34 +26,12 @@ class TestMovies(unittest.TestCase):
 		self.assertTrue(self.is_json(r.content.decode()))
 		resp = json.loads(r.content.decode())
 		self.assertEqual(resp['result'],'error')
+	
+	def test_users_delete(self):
+		self.assertEqual('1','1')
 
 	def test_users_put(self):
 		self.assertEqual('1','1')
-		'''self.reset_data()
-		user = 'Andy'
-
-		u = {}
-		u['pwd'] = 'animep' '''
-		#r = requests.post(self.
-		# TODO CHECK INFO TEST
-		'''self.assertTrue(self.is_json(r.content.decode('utf-8')))
-		resp = json.loads(r.content.decode('utf-8'))
-		self.assertEqual(resp['title'], 'Broken Arrow (1996)')
-		self.assertEqual(resp['genres'], 'Action|Thriller')
-
-		m = {}
-		m['title'] = 'ABC'
-		m['genres'] = 'Sci-Fi|Fantasy'
-		r = requests.put(self.MOVIES_URL + str(movie_id), data = json.dumps(m))
-		self.assertTrue(self.is_json(r.content.decode('utf-8')))
-		resp = json.loads(r.content.decode('utf-8'))
-		self.assertEqual(resp['result'], 'success')
-
-		r = requests.get(self.MOVIES_URL + str(movie_id))
-		self.assertTrue(self.is_json(r.content.decode('utf-8')))
-		resp = json.loads(r.content.decode('utf-8'))
-		self.assertEqual(resp['title'], m['title'])
-		self.assertEqual(resp['genres'], m['genres'])'''
 	
 	def test_users_post(self):
 		self.reset_data()
@@ -69,6 +48,9 @@ class TestMovies(unittest.TestCase):
 		# TODO GET TEST
 		#r = requests.get(self.USERS_URL)
 		
+	def test_users_password_put(self):
+		self.assertEqual('1','1')
+
 	def test_users_wallet_post(self):
 		self.reset_data()
 		u = {}
@@ -92,7 +74,6 @@ class TestMovies(unittest.TestCase):
 		resp = json.loads(r.content.decode())
 		self.assertEqual(resp['result'],'success')
 		
-		# TODO GET WALLET
 		r = requests.get(self.USERS_URL + URL) 
 		self.assertTrue(self.is_json(r.content.decode()))
 		resp = json.loads(r.content.decode())
@@ -101,21 +82,35 @@ class TestMovies(unittest.TestCase):
 		l.append(assetdict)
 		self.assertEqual(resp['wallet'],l)
 
-	def test_movies_delete(self):
+	def test_user_delete(self):
 		self.assertEqual('1','1')
-		'''self.reset_data()
-		movie_id = 95
 
-		m = {}
-		r = requests.delete(self.MOVIES_URL + str(movie_id), data = json.dumps(m))
-		self.assertTrue(self.is_json(r.content.decode('utf-8')))
-		resp = json.loads(r.content.decode('utf-8'))
-		self.assertEqual(resp['result'], 'success')
+	def test_crypto_hottest_put(self):
+		d = {}
+		d['temp'] = 'hot'
+		d['days'] = 10
+		d['count'] = 5
+		d['static'] = True
 
-		r = requests.get(self.MOVIES_URL + str(movie_id))
-		self.assertTrue(self.is_json(r.content.decode('utf-8')))
-		resp = json.loads(r.content.decode('utf-8'))
-		self.assertEqual(resp['result'], 'error')'''
+		r = requests.put(self.CRYPTO_URL, data = json.dumps(d))
+		self.assertTrue(self.is_json(r.content.decode()))
+		resp = json.loads(r.content.decode())
+		self.assertEqual(resp,{'result':'success','mode':'hot','BCH':'40.97%','XRP':'16.44%','XLM':'11.80%','DASH':'8.67%','ETH':'7.71%'})
+
+	def test_crypto_if_put(self):
+		d = {}
+		assetdict ={}
+		assetdict['BTC']=2
+		assetdict['DBC']=100
+		assetlist = []
+		assetlist.append(assetdict)
+		d['asset']=assetlist
+		
+		days = '100'
+		r = requests.put(self.CRYPTO_URL + days, data = json.dumps(d))
+		self.assertTrue(self.is_json(r.content.decode()))
+		resp = json.loads(r.content.decode())
+		self.assertEqual(resp,{'result':'success',"Net Profit": "-3327.38", "Net Percentage": "-20.24", "breakdown": [{"crypto": "BTC", "profit": "-3325.26", "profit percentage": "-20.23"}, {"crypto": "DBC", "profit": "-2.12", "profit percentage": "-73.81"}]})
 
 if __name__ == "__main__":
     unittest.main()
