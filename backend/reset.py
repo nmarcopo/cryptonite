1cccf239d4722deacd811e239f7f4d2e6c34bfc5
SungHyun Shin <sshin1@student04.cse.nd.edu>
2018-11-28 00:30:21 -0500

    removed line

--- backend/reset.py
+++ backend/reset.py
@@ -8,7 +8,6 @@ class ResetController:
 	def __init__(self, userdatabase):
 		self.udb = userdatabase
 	
-    # TODO should be a put
 	def GET(self):
 		self.udb.reset_data()
 		return json.dumps({'result':'success'}) 
