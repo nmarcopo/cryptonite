ea580fe5967c6a8b789afcba7b4645dfc578ebf5
Administrator <csong1@nd.edu>
2018-12-01 19:08:36 -0500

    support real time data

--- backend/crypto.py
+++ backend/crypto.py
@@ -17,12 +17,14 @@ class CryptoController:
             days = data['days']
             count = data['count']
             static = data['static']
+            preloaded = None
             if static:
                 with open('hotcold.dat') as f:
-                    preloaded = f.readline().strip()
+                    data = f.readline().strip()
+                    preloaded = json.loads(data)
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            response = loop.run_until_complete(self.crypto.find_hottest_coldest(days, count, temp, json.loads(preloaded)))
+            response = loop.run_until_complete(self.crypto.find_hottest_coldest(days, count, temp, preloaded))
 
             output = json.loads(response)
             output['result'] = 'success'
@@ -37,11 +39,13 @@ class CryptoController:
             payload = cherrypy.request.body.read()
             data = json.loads(payload)
             asset = data['asset'][0]                # data['asset'] is a list
+            preloaded = None
             with open('whatif.dat') as f:
-                preloaded = f.readline().strip()
+                data = f.readline().strip()
+                preloaded = json.loads(data)
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            response = loop.run_until_complete(self.crypto.what_if_investment(int(days), asset, json.loads(preloaded)))
+            response = loop.run_until_complete(self.crypto.what_if_investment(int(days), asset, preloaded))
 
             output = json.loads(response)
             output['result'] = 'success'
